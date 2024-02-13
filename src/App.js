df16aae2b964494cd6656059ebc5d97a313c595a
Codetraker <swarajbibhuprasad@gmail.com>
2024-02-13 13:23:51 +0530

    add about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import About from './components/Layout/AboutBar/About';
 import Header from './components/Layout/Header';
-import Card from './components/UI/Card';
+
 
 function App() {
   return (
     <>
     <Header />
-    <Card>Absolute Delicious</Card>
-    <h2>Biryani, BBQ, Kebab, North Indian, Desserts</h2>
+    <About />
     </>
   );
 }
---
+++ src/components/Layout/AboutBar/About.js
@@ -0,0 +1,11 @@
+import Card from '../../UI/Card';
+import classes from './About.module.css';
+
+const About = props => {
+    return (
+        <Card className={classes.about}>
+            <h1>hello</h1>
+        </Card>
+    );
+}
+export default About;
